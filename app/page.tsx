de8b82fe057c9bcd9ced2c3d4d63cefd88227a77
georgwindhaber <georg.windhaber@gmail.com>
2023-07-30 21:27:40 +0200

    Add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,70 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Explanation</h1>
-      <h4 className={styles.subtitle}>This is a demo of a bug in Next.js.</h4>
-      <p>
+    <main>
+      <h1 className="text-6xl md:text-9xl font-bold p-5 text-center">
+        Explanation
+      </h1>
+      <h4 className="text-2x text-center">
+        This is a demo of a bug in Next.js.
+      </h4>
+      <p className="p-5">
         What happens: When you click on a image, which is wrapped in a link,
         brings you to a page. Now, on mobile, when you click the back button{" "}
-        <span className="italics">and then scroll on the images</span>, you get
-        redirectet <span className="italics">back</span> to the initial clicked
+        <span className="italic">and then scroll on the images</span>, you get
+        redirectet <span className="italic">back</span> to the initial clicked
         post.
       </p>
-      <div className={styles.grid}>
+      <div className="grid grid-cols-3 gap-5 p-5">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => {
           return (
-            <Link className={styles.link} href={`/posts/${i}`} key={i}>
+            <Link
+              className="relative aspect-square"
+              href={`/posts/${i}`}
+              key={i}
+            >
               <Image
-                key={i}
                 src={`/imgs/${i}.jpg`}
                 fill
                 sizes="30vw"
                 alt={"img" + i}
+                className="rounded-lg object-contain"
               />
             </Link>
           );
         })}
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => {
           return (
-            <Link className={styles.link} href={`/posts/${i}`} key={i}>
+            <Link
+              className="relative aspect-square"
+              href={`/posts/${i}`}
+              key={i}
+            >
               <Image
-                key={i}
                 src={`/imgs/${i}.jpg`}
                 fill
                 sizes="30vw"
                 alt={"img" + i}
+                className="rounded-lg object-contain"
               />
             </Link>
           );
         })}
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => {
           return (
-            <Link className={styles.link} href={`/posts/${i}`} key={i}>
+            <Link
+              className="relative aspect-square"
+              href={`/posts/${i}`}
+              key={i}
+            >
               <Image
-                key={i}
                 src={`/imgs/${i}.jpg`}
                 fill
                 sizes="30vw"
                 alt={"img" + i}
+                className="rounded-lg object-contain"
               />
             </Link>
           );
--- app/posts/[index]/page.tsx
+++ app/posts/[index]/page.tsx
@@ -1,11 +1,10 @@
-import styles from "./post.module.css";
-
 export default async function Post({ params }: { params: { index: number } }) {
   return (
     <>
-      <h1 className={styles.center}>Post {params.index}</h1>
-      <p className={styles.center}>
-        There is no back button, use your mobile device native
+      <h1 className="text-center">Post {params.index}</h1>
+      <p className="p-5 text-center">
+        There is no back button, use your mobile device native way (swipe back
+        most of the times)
       </p>
     </>
   );
